84a308efc56a52946ca48a795e1133b6bf5e3520
qmz123 <984538988@qq.com>
2020-01-04 11:47:36 +0800

    哈哈

---
+++ src/main/java/com/ryit/servlet/MyController.java
@@ -0,0 +1,19 @@
+package com.ryit.servlet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2019/12/28.
+ */
+@Controller
+@RequestMapping("/")
+public class MyController {
+
+    @RequestMapping("hello")
+    public String insert(){
+        System.out.println("访问成功");
+        return "/WEB-INF/qmz.jsp";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/qmz.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2019/12/28
+  Time: 11:04
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    success
+</body>
+</html>
